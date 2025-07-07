75c0c6d9854d0a66063bb56ae041efccdc800f59
shameem <shameem4contact@gmail.com>
2025-07-07 16:11:38 +0530

    Js

--- Day1/1.js
+++ Day1/1.js
@@ -1,45 +1,51 @@
 console.log("hii");
-a=10;
-b="string";
-c=3.5;
-d='a';
-e=true;
+a = 10;
+b = "string";
+c = 3.5;
+d = 'a';
+e = true;
 
 
-var x=10;
-var x=20;
-x=50;
-console.log(x);
+var x = 10;
+var x = 20;
+x = 50;
+console.log("x:", x);
 
-let y=10;
-y=30;
+let y = 10;
+y = 30;
 
-const z=10;
+const z = 10;
 // z=50;
 // let y = 20;
 // const z=30;
 
 
-var add=a+b;
-var a=10;
-var b=2;
-add =a+b;
-sub =a-b;
-mul =a*b;
-div =a/b;
-mod =a%b;
-pow =b**10;
-b**=b
-a=5;
-b="56"
-var g=a==b?"equal":a!=b ?"not equal":"equal";
+var add = a + b;
+var a = 10;
+var b = 2;
+add = a + b;
+sub = a - b;
+mul = a * b;
+div = a / b;
+mod = a % b;
+pow = b ** 10;
+b **= b
+a = 5;
+// b="5"
+var g = a == b ? "equal" : a != b ? "not equal" : "equal";
+var i = a === b;
+console.log("the value of a is", a, "the value of b is", b);
+console.log("sub: " + sub);
+console.log("mul: " + mul);
+console.log("div: " + div);
+console.log("mod:" + mod);
+console.log("pow: " + pow);
+console.log("g: " + g);
+console.log("Is the value and datatype is equal : ", a === b);
+x = 10;
+y = "hi";
+v = y + x;
+u = x + y;
+console.log("we can add string to numnber: " + v);
+console.log("we can add number to string: " + u);
 
-console.log ("the value of a is "+a+" the value of b is"+b);
-console.log (sub);
-console.log (mul);
-console.log(div);
-console.log (mod);
-console.log (pow);
-console.log (g);
-console.log (a==b);
-a=100;
