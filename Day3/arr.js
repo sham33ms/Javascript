e1ec30baae189a677f075a5070d32f1f652e7517
shameem <shameem4contact@gmail.com>
2025-07-11 16:04:41 +0530

    Day4

---
+++ Day3/arr.js
@@ -0,0 +1,15 @@
+// array creation literal method
+var arr=[1,2,3,4,5];
+// array constructir
+var arr1 = new Array(1,2,3,4,5,6)
+console.log(arr , arr1);
+// indexing and replacinng
+console.log(arr[0], arr[1]);
+//modufng arr
+arr[0]=10;
+console.log(arr);
+
+// sorting
+const arr2 = [67,63,25,99,7,9,73,5];
+arr2.sort((a,b)=>a-b);
+console.log(arr2);
